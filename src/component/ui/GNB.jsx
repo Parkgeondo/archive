d1c8af1a8976d4bcb8f76334fb3327ab60d57ac4
park geon do <wksehs123@naver.com>
2023-06-20 21:33:43 +0900

    first commit

--- src/component/ui/GNB.jsx
+++ src/component/ui/GNB.jsx
@@ -25,7 +25,7 @@ function TopUi(props){
                 <div className={styles.Logo}>
                 <label class="boxradio">
                     <input type="radio" name="shape" />
-                    <img src={main?logo2:logo} alt="" onClick={() => {nav("/");
+                    <img src={main?logo2:logo} alt="/archive" onClick={() => {nav("/");
             }}/>
                 </label>
                 </div>
